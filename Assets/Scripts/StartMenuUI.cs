bb844d269d41d5da2421241cf625aa303e8eaddd
Nacho <74147757+nachommarti@users.noreply.github.com>
2026-01-09 17:03:03 +0100

    Tutorial screen

--- Assets/Scripts/StartMenuUI.cs
+++ Assets/Scripts/StartMenuUI.cs
@@ -5,7 +5,7 @@ public class StartMenuUI : MonoBehaviour
 { 
     public void StartGame()
     {
-        SceneManager.LoadScene("Lvl_Nivelin 1");
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void ExitGame()
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    public void OK()
+    {
+        SceneManager.LoadScene("Lvl_Nivelin 1");
+    }
+}
